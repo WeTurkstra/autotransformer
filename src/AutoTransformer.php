b375518858f8082ae8afa7e9df832d5bb5f764fb
Willem Turkstra <willemturkstra@gmail.com>
2024-05-05 13:21:09 +0200

    Added access to parent object properties

--- src/AutoTransformer.php
+++ src/AutoTransformer.php
@@ -4,30 +4,29 @@ namespace Tibisoft\AutoTransformer;
 
 use Tibisoft\AutoTransformer\Attribute\AttributeHandlerInterface;
 use Tibisoft\AutoTransformer\Attribute\BaseAttribute;
-use Tibisoft\AutoTransformer\Attribute\Count;
-use Tibisoft\AutoTransformer\Attribute\Synonyms;
 use Tibisoft\AutoTransformer\Exception\TransformException;
 
 class AutoTransformer implements AutoTransformerInterface
 {
     public function transform(object $from, string|object $to): object
     {
-        $reflectionFrom = new \ReflectionClass($from);
         $reflectionTo = new \ReflectionClass($to);
         if (!is_object($to)) {
             $to = $reflectionTo->newInstanceWithoutConstructor();
         }
 
+        $reflectionFrom = new \ReflectionClass($from);
+
         foreach ($reflectionTo->getProperties() as $property) {
             //look for attribute
             $attributes = $property->getAttributes();
 
-            /** @var \ReflectionAttribute $attribute */
             foreach ($attributes as $attribute) {
                 $attribute = $attribute->newInstance();
                 if (!($attribute instanceof BaseAttribute)) {
                     continue;
                 }
+
                 if (class_exists($attribute->isHandledBy())) {
                     /** @var AttributeHandlerInterface $handler */
                     $handler = $attribute->isHandledBy();
@@ -58,14 +57,40 @@ class AutoTransformer implements AutoTransformerInterface
         $propertyName = ($propertyName === '') ? $property->getName() : $propertyName;
 
         try {
-            if (!$reflectionFrom->hasProperty($propertyName)) {
+            if (!$this->hasProperty($reflectionFrom, $propertyName)) {
                 return;
             }
 
-            $propertyFrom = $reflectionFrom->getProperty($propertyName);
+            $propertyFrom = $this->getProperty($reflectionFrom, $propertyName);
             $property->setValue($to, $propertyFrom->getValue($from));
         } catch (\ReflectionException|\TypeError $typeError) {
             throw new TransformException($typeError->getMessage());
         }
+    }
+
+    private function getProperty(\ReflectionClass $reflectionClass, string $propertyName): \ReflectionProperty
+    {
+        try {
+            return $reflectionClass->getProperty($propertyName);
+        } catch (\ReflectionException|\TypeError $typeError) {
+            if ($reflectionClass->getParentClass() !== false) {
+                return $reflectionClass->getParentClass()->getProperty($propertyName);
+            }
+
+            throw $typeError;
+        }
+    }
+
+    private function hasProperty(\ReflectionClass $reflectionClass, string $propertyName): bool
+    {
+        if ($reflectionClass->hasProperty($propertyName)) {
+            return true;
+        }
+
+        if ($reflectionClass->getParentClass() !== false) {
+            return $reflectionClass->getParentClass()->hasProperty($propertyName);
+        }
+
+        return false;
     }
 }
