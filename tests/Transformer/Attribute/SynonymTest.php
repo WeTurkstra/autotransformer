c0237181efdb8848209d32659486f2b11d441a5b
Willem Turkstra <willemturkstra@gmail.com>
2024-05-05 13:20:37 +0200

    Added test synonyms attribute

---
+++ tests/Transformer/Attribute/SynonymTest.php
@@ -0,0 +1,48 @@
+<?php
+
+$object1 = new SynonymsClass('Willem');
+$object2 = new SynonymsClass('Diego');
+
+$object3 = new Synonyms1Class('Bianca');
+$object4 = new Synonyms1Class('Mariska');
+
+test('Test synonym Attribute', function ($object, $expected) {
+
+    $transformer = new \Tibisoft\AutoTransformer\AutoTransformer();
+
+    /** @var SynonymsDTO $dto */
+    $dto = $transformer->transform($object, SynonymsDTO::class);
+
+    expect($dto->username)->toBe($expected);
+})->with([
+    [$object1, 'Willem'],
+    [$object2, 'Diego'],
+    [$object3, 'Bianca'],
+    [$object4, 'Mariska'],
+
+]);
+
+class SynonymsClass
+{
+    public function __construct(public string $name)
+    {
+
+    }
+}
+class Synonyms1Class
+{
+    public function __construct(public string $lastname)
+    {
+
+    }
+}
+
+class SynonymsDTO
+{
+    public function __construct(
+        #[\Tibisoft\AutoTransformer\Attribute\Synonyms(synonyms: ['lastname', 'name'])]
+        public string $username)
+    {
+
+    }
+}
